586e25422aec931f1799c21a9f77cb53b7e596ab
Rahul Bhargava <rahulbot@gmail.com>
2023-09-19 11:13:00 -0400

    version bump for testing

--- processor/__init__.py
+++ processor/__init__.py
@@ -9,7 +9,7 @@ from sentry_sdk.integrations.logging import ignore_logger
 from sentry_sdk import init
 from typing import Dict
 
-VERSION = "3.6.0b6"
+VERSION = "3.6.0b7"
 SOURCE_GOOGLE_ALERTS = "google-alerts"
 SOURCE_MEDIA_CLOUD = "media-cloud"
 SOURCE_NEWSCATCHER = "newscatcher"
