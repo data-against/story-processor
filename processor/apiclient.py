76977aa8a22de90a3323fb2fd684b5817fe91d42
Rahul Bhargava <rahulbot@gmail.com>
2023-09-26 11:39:08 -0400

    clean up API use and timeout

--- processor/apiclient.py
+++ processor/apiclient.py
@@ -9,7 +9,7 @@ def get_projects_list() -> Dict:
     The main server holds configuraiton and models - get the list.
     :return:
     """
-    path = FEMINICIDE_API_URL + '/api/story_processor/projects.json'
+    path = FEMINICIDE_API_URL + 'api/story_processor/projects.json'
     data = _get_json(path)
     for p in data:
         p['search_terms'] = p['search_terms'].replace(u'\xa0', u' ')
@@ -21,12 +21,12 @@ def get_language_models_list() -> Dict:
     Each project can refer to one or two models - get them all.
     :return:
     """
-    path = FEMINICIDE_API_URL + '/api/story_processor/language_models.json'
+    path = FEMINICIDE_API_URL + 'api/story_processor/language_models.json'
     return _get_json(path)
 
 
 def _get_json(path: str) -> Dict:
     params = dict(apikey=FEMINICIDE_API_KEY)
     r = requests.get(path, params,
-                     timeout=(3.05*20)*10)  # docs say set it to slightly larger than a multiple of 3, so 10ish minutes
+                     timeout=(3.05*20)*5)  # docs say set it to slightly larger than a multiple of 3, so Nish minutes
     return r.json()
