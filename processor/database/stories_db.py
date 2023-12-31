fdd8d83b3d079925be4df012128c0afe6ae9a95f
Rahul Bhargava <rahulbot@gmail.com>
2023-12-31 14:27:53 -0500

    refactor(db): insert stories 1-by-1 to ignore duplicates violating unique project/url constraint

--- processor/database/stories_db.py
+++ processor/database/stories_db.py
@@ -4,6 +4,7 @@ import logging
 from typing import Dict, List
 
 from sqlalchemy import delete, select, text, update
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.session import Session
 from sqlalchemy.sql import func
 
@@ -23,6 +24,7 @@ def add_stories(
     :param source:
     :return: list of ids of objects inserted
     """
+    ignored_count = 0
     new_source_story_list = copy.copy(source_story_list)
     now = dt.datetime.now()
     for discovered_story in new_source_story_list:
@@ -33,8 +35,18 @@ def add_stories(
         db_story.above_threshold = False
         discovered_story["db_story"] = db_story
     # now insert in batch to the database
-    session.add_all([s["db_story"] for s in new_source_story_list])
-    session.commit()
+    for s in new_source_story_list:
+        try:
+            session.add(s["db_story"])
+            session.commit()
+        except IntegrityError:
+            # duplicate story, so just ignore it
+            session.rollback()
+            logger.debug(
+                f"Duplicate story found for project {project['id']}: {s['url']}"
+            )
+            del s["db_story"]
+            ignored_count += 1
     # only keep ones that inserted correctly
     new_source_story_list = [
         s for s in new_source_story_list if ("db_story" in s) and s["db_story"].id
@@ -46,6 +58,7 @@ def add_stories(
     for s in new_source_story_list:  # free the DB objects back for GC
         if "db_story" in s:  # a little extra safety
             del s["db_story"]
+    logger.info(f"  ignored {ignored_count} stories as duplicates")
     return new_source_story_list
 
 
--- processor/test/test_tasks.py
+++ processor/test/test_tasks.py
@@ -5,20 +5,11 @@ import unittest
 import processor.database as database
 import processor.tasks as tasks
 from processor import SOURCE_MEDIA_CLOUD
-from processor.test import test_fixture_dir
+from processor.test import sample_story_ids, test_fixture_dir
 from processor.test.test_projects import TEST_EN_PROJECT
 
 
 class TestTasks(unittest.TestCase):
-    def _sample_story_ids(self):
-        # this loads read data from a real request from a log file on the real server
-        with open(
-            os.path.join(test_fixture_dir, "aapf_samples.json"), encoding="utf-8"
-        ) as f:
-            sample_stories = json.load(f)
-        story_ids = [s["id"] for s in sample_stories]
-        return story_ids
-
     def _classify_story_ids(self, project, story_ids):
         stories_with_text = []
         for sid in story_ids:
@@ -41,7 +32,7 @@ class TestTasks(unittest.TestCase):
     def test_add_probability_to_stories(self):
         project = TEST_EN_PROJECT.copy()
         project["language_model_id"] = 3
-        story_ids = self._sample_story_ids()
+        story_ids = sample_story_ids()
         classified_stories = self._classify_story_ids(project, story_ids)
         matching_stories = [s for s in classified_stories if s["id"] == 1957814773]
         assert len(matching_stories) == 1
@@ -59,7 +50,7 @@ class TestTasks(unittest.TestCase):
             # assert round(classified_stories[0]['confidence'], 5) == 0.78392
 
     def test_add_entities_to_stories(self):
-        story_ids = self._sample_story_ids()
+        story_ids = sample_story_ids()
         stories_with_text = []
         for sid in story_ids:
             with open(
