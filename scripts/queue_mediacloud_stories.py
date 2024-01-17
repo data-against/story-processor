591ae0f53f25f3675a1298c553837337cc6fa729
Rahul Bhargava <rahulbot@gmail.com>
2024-01-17 13:42:01 -0500

    refactor(mc): enable another pilot MC project

--- scripts/queue_mediacloud_stories.py
+++ scripts/queue_mediacloud_stories.py
@@ -45,7 +45,7 @@ def load_projects_task() -> List[Dict]:
     logger.info("  Checking {} projects".format(len(project_list)))
     # return [p for p in project_list if p['id'] == 154]
     return [
-        p for p in project_list if p["id"] in [147, 23, 21]
+        p for p in project_list if p["id"] in [147, 23, 21, 170]
     ]  # selected list of close collaborators from Patti
     # return project_list
 
