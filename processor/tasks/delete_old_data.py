2fd2661123384e0c950182c859e794d82f9afe5b
Walla <127447783+math4humanities@users.noreply.github.com>
2024-08-01 12:48:20 -0400

    time-change

--- processor/tasks/delete_old_data.py
+++ processor/tasks/delete_old_data.py
@@ -17,7 +17,7 @@ app.conf.beat_schedule.update(
         "delete-old-stories": {
             "task": "processor.tasks.delete_old_stories_task",
             "schedule": crontab(
-                day_of_week="*", hour="11", minute="31"
+                day_of_week="*", hour="7", minute="31"
             ),  # Every night at midnight do some cleaning
             "args": (30,),
         },
