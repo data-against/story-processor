f76f568a8384fe50bf459a5484897eba3d872a69
math4humanities <rahama.w@northeastern.edu>
2024-08-07 12:45:40 -0400

    docs(run-workers): Add documentation for additional worker options, allow -P solo to be optional for Windows users

--- run-workers.sh
+++ run-workers.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-celery -A processor worker -l info --concurrency=4 -E -P solo
+# -E, send task events that can be captured by monitors and celery log
+# -P solo (optional), implement solo pools since Windows does not allow process forking only spawning
+celery -A processor worker -l info --concurrency=4 -E # -P solo
 
