6532b073e73dd407c9ecf74d99593aae28c2517c
Rahul Bhargava <rahulbot@gmail.com>
2024-11-07 14:32:02 -0500

    fix(models): try new BERT base model download URLS

--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -3,9 +3,9 @@
 python -m scripts.download_models
 
 # English language LR model
-curl -SL https://storage.googleapis.com/tfhub-modules/google/universal-sentence-encoder/4.tar.gz -o 4.tar.gz
-tar xfz 4.tar.gz -C files/models/embeddings-en
+curl -SL https://tfhub.dev/google/universal-sentence-encoder/2?tf-hub-format=compressed -o use2.tar.gz
+tar xfz use2.tar.gz -C files/models/embeddings-en
 
 # Multilingual language LR model (for Korean)
-curl -SL https://storage.googleapis.com/tfhub-modules/google/universal-sentence-encoder-multilingual/3.tar.gz -o 3.tar.gz
-tar xfz 3.tar.gz -C files/models/embeddings-multi
+curl -SL https://www.kaggle.com/api/v1/models/google/universal-sentence-encoder/tensorFlow2/multilingual/2/download -o usem2.tar.gz
+tar xfz usem2.tar.gz -C files/models/embeddings-multi
