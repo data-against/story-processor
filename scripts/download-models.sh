15f1a5a97479a314bbbcb80621acfb1bb4eaa05b
Rahul Bhargava <rahulbot@gmail.com>
2024-11-07 21:54:34 -0500

    fix(models): use our own hosted embeddings models

--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-python -m scripts.download_models
+#python -m scripts.download_models
 
 # English language LR model
-curl -SL https://tfhub.dev/google/universal-sentence-encoder/2?tf-hub-format=compressed -o use2.tar.gz
-tar xfz use2.tar.gz -C files/models/embeddings-en
+# originally: curl -SL https://storage.googleapis.com/tfhub-modules/google/universal-sentence-encoder/4.tar.gz -o 4.tar.gz
+curl -SL "https://www.dropbox.com/s/6vfe4ttvoypisp5/embeddings-en.tar.gz?dl=1" -o embeddings-en.tar.gz
+tar xfz embeddings-en.tar.gz -C files/models/
 
 # Multilingual language LR model (for Korean)
-curl -SL https://www.kaggle.com/api/v1/models/google/universal-sentence-encoder/tensorFlow2/multilingual/2/download -o usem2.tar.gz
-tar xfz usem2.tar.gz -C files/models/embeddings-multi
+# originally: curl -SL https://storage.googleapis.com/tfhub-modules/google/universal-sentence-encoder-multilingual/3.tar.gz -o 3.tar.gz
+curl -SL "https://www.dropbox.com/scl/fi/wrpg8tqgqb9boqgwf07ec/embeddings-multi.tar.gz?rlkey=rxtqq8ggfls4jh00rrdurs5wx&dl=1" -o embeddings-multi.tar.gz
+tar xfz embeddings-multi.tar.gz -C files/models/
