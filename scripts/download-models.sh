d0a02aed54c5993786d5f360e929d1516e9b85da
Rahul Bhargava <rahulbot@gmail.com>
2024-11-07 22:22:30 -0500

    fix(models): fix download script to get models again

--- scripts/download-models.sh
+++ scripts/download-models.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#python -m scripts.download_models
+python -m scripts.download_models
 
 # English language LR model
 # originally: curl -SL https://storage.googleapis.com/tfhub-modules/google/universal-sentence-encoder/4.tar.gz -o 4.tar.gz
